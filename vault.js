c3ff4f4340a082268c92d2b77c87b87cf1ffdac5
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-07-03 17:39:49 -1000

    resolves tests 1 and 2

--- vault.js
+++ vault.js
@@ -1,4 +1,26 @@
 'use strict';
 module.exports = function() {
-  
+
+    let vault = {};
+
+    function setValue(key, value){
+        // vault.key = value;
+        // vault[key] = value;
+        Object.assign(vault, {key: value});
+    };
+
+    function getValue(key){
+        if (key === undefined){
+            return null;
+        } else if (!(key in vault)){
+            return null;
+        }
+        return vault.key;
+    };
+
+    return {
+        setValue: setValue,
+        getValue: getValue
+    };
+
 };
